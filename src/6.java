76b10269f163f09824b7c2d6f2c0b30e78a52b7d
cuelloalba507 <cuelloalba507@gmail.com>
2025-03-11 00:13:39 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SchoolProject {
+  public static void main(String[] args) {
+    List<Integer> numbers = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      numbers.add(i);
+    }
+    System.out.println(numbers);
+  }
+}
