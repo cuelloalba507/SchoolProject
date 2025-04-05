d32f3387d181aa863fd33faaf95a9dc4b0b37c13
cuelloalba507 <cuelloalba507@gmail.com>
2025-04-05 06:50:38 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SimpleJavaProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello World!");
+        
+        // You can add more code here as needed
+        
+        scanner.close();
+    }
+}
