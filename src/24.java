a0b6eb91533a66e8c77a988221927389ceaa53fe
cuelloalba507 <cuelloalba507@gmail.com>
2025-04-05 05:31:19 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,21 @@
+public class SimpleJavaCode {
+    public static void main(String[] args) {
+        // Basic arithmetic operations
+        System.out.println("Sum: " + (20 + 30));
+        System.out.println("Difference: " + (40 - 15));
+        System.out.println("Product: " + (20 * 30));
+        System.out.println("Quotient: " + (20 / 5));
+
+        // Simple Java loop
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        // Conditionally execute code based on a condition
+        if (true) {
+            System.out.println("Condition is true!");
+        } else {
+            System.out.println("Condition is false!");
+        }
+    }
+}
