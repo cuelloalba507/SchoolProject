f62a149d20de54441e7b5a81a7edc0757c91210b
cuelloalba507 <cuelloalba507@gmail.com>
2025-04-09 05:04:59 +0000

    Add 28.java

---
+++ src/28.java
@@ -0,0 +1,8 @@
+public class SimpleJavaCode {
+    public static void main(String[] args) {
+        // Example of using variables in Java
+        String name = "Alice";
+        int age = 20;
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+}
