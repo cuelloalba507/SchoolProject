e32d327d92a9815561958956b00fdfa16531b95b
cuelloalba507 <cuelloalba507@gmail.com>
2025-04-23 10:53:49 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,14 @@
+public class SimpleRepository {
+    private String name;
+    private int age;
+
+    public SimpleRepository(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void addStudent(Student student) {
+        // Add student to the repository
+        // ...
+    }
+}
